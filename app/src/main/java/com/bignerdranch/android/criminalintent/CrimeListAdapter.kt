32bb7bd36e4acb427e7bd2af8a8ecc375969f1dd
KingofGuava <arthurlin0831@gmail.com>
2024-03-05 00:14:36 -0500

    hw4 recycler view

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
@@ -1,14 +1,16 @@
 package com.bignerdranch.android.criminalintent
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.criminalintent.databinding.ListItemCrimeBinding
 
-class CrimeHolder(
-    private val binding: ListItemCrimeBinding
-) : RecyclerView.ViewHolder(binding.root) {
+class CrimeHolder(private val binding: ListItemCrimeBinding) :
+    RecyclerView.ViewHolder(binding.root) {
     fun bind(crime: Crime) {
         binding.crimeTitle.text = crime.title
         binding.crimeDate.text = crime.date.toString()
@@ -23,21 +25,58 @@ class CrimeHolder(
     }
 }
 
-class CrimeListAdapter(
-    private val crimes: List<Crime>
-) : RecyclerView.Adapter<CrimeHolder>() {
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): CrimeHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val binding = ListItemCrimeBinding.inflate(inflater, parent, false)
-        return CrimeHolder(binding)
+class SeriousCrimeHolder(view: View) :
+    RecyclerView.ViewHolder(view) {
+    private val crimeTitle = view.findViewById<TextView>(R.id.scrime_title)
+    private val crimeDate = view.findViewById<TextView>(R.id.scrime_date)
+    private val actionButton = view.findViewById<Button>(R.id.actionButton)
+    init {
+        view.setOnClickListener {
+            Toast.makeText(
+                view.context,
+                "crime clicked!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
+    fun bind(crime: Crime) {
+        crimeTitle.setText(crime.title)
+        crimeDate.setText(crime.date.toString())
 
-    override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
-        val crime = crimes[position]
-        holder.bind(crime)
+    }
+}
+
+class CrimeListAdapter(private val crimes: List<Crime>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+            RecyclerView.ViewHolder {
+                val inflater = LayoutInflater.from(parent.context)
+
+                if(viewType == 1){
+                    val binding = ListItemCrimeBinding.inflate(inflater, parent, false)
+                    return CrimeHolder(binding)
+                }
+                else {
+                    //val binding = SeriousCrimeBinding.inflate(inflater, parent, false)
+                    val view = inflater.inflate(R.layout.serious_crime, parent, false)
+                    return SeriousCrimeHolder(view)
+                }
+            }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            val crime = crimes[position]
+
+            if(holder is CrimeHolder){
+                holder.bind(crime)
+            }
+            else if(holder is SeriousCrimeHolder){
+                holder.bind(crime)
+            }
+
+        }
+
+    override fun getItemViewType(position: Int): Int {
+        return crimes[position].viewType
     }
 
     override fun getItemCount() = crimes.size
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -10,14 +10,26 @@ class CrimeListViewModel : ViewModel() {
 
     init {
         for (i in 0 until 100) {
-            val crime = Crime(
-                id = UUID.randomUUID(),
-                title ="Crime #$i",
-                date = Date(),
-                isSolved = i % 2 == 0
-            )
-
-            crimes += crime
+            if(i % 3 == 0 || i % 7 == 0){
+                val crime = Crime(
+                    id = UUID.randomUUID(),
+                    title ="Crime #$i",
+                    date = Date(),
+                    isSolved = i % 2 == 0,
+                    viewType = 1
+                )
+                crimes += crime
+            }
+            else{
+                val crime = Crime(
+                    id = UUID.randomUUID(),
+                    title ="Serious Crime #$i",
+                    date = Date(),
+                    isSolved = i % 2 == 0,
+                    viewType = 2
+                )
+                crimes += crime
+            }
         }
     }
 }
